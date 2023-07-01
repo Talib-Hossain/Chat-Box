452d8d4929d24033e190d1f28ba320a6f6e80569
Talib-Hossain <talibhossain6@gmail.com>
2023-07-01 19:32:58 +0530

    Updated Feature Profile Picture 4

--- app/src/main/java/com/example/chatbox/UpdateProfile.kt
+++ app/src/main/java/com/example/chatbox/UpdateProfile.kt
@@ -1,7 +1,11 @@
 package com.example.chatbox
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
@@ -65,6 +69,22 @@ class UpdateProfile : AppCompatActivity() {
 
         })
 
+        selectProfilePicture.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 10)
+        }
 
+    }
+
+    var selectedPhotoUri: Uri? = null
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 10 && resultCode == Activity.RESULT_OK && data != null) {
+            // proceed and check what the selected image was....
+            selectedPhotoUri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+            profileImageView.setImageBitmap(bitmap)
+        }
     }
 }
